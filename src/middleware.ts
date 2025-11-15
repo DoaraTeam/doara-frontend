0424ab6228990dd56487f552c1533c2a11725987
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2025-11-15 14:59:01 +0700

    style: format code for consistency in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,16 @@
-import createMiddleware from 'next-intl/middleware';
-import {routing} from './i18n/routing';
-import {NextRequest, NextResponse} from 'next/server';
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
 const intlMiddleware = createMiddleware(routing);
 
 export default function middleware(request: NextRequest) {
-  const {pathname} = request.nextUrl;
-  if (pathname.startsWith('/admin')) {
+  const { pathname } = request.nextUrl;
+  if (pathname.startsWith("/blog")) {
     return NextResponse.next();
   }
   return intlMiddleware(request);
 }
 export const config = {
-  matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)']
+  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
 };
